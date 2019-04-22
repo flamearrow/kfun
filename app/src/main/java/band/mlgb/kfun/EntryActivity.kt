0f03233e40bbeb55b71a011205f2f531bface45d
Chen Cen <arrow.cen@gmail.com>
2019-04-21 17:35:48 -0700

    init commit

---
+++ app/src/main/java/band/mlgb/kfun/EntryActivity.kt
@@ -0,0 +1,17 @@
+package band.mlgb.kfun
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+
+class EntryActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.entry_activity)
+    }
+
+    fun imageLabeling(view: View) {
+        startActivity(Intent(applicationContext, ImageLabelingActivity::class.java))
+    }
+}
